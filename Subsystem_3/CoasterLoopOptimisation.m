9780aa84a52ce8db44ae2f08474bebfca482bcf2
Oli Thompson <olithompson@rocketmail.com>
2019-12-13 16:00:31 +0000

    changes

--- Subsystem_3/CoasterLoopOptimisation.m
+++ Subsystem_3/CoasterLoopOptimisation.m
@@ -1,35 +1,14 @@
-tic
-options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
-x0 = [1.5,30,0.10];
+tic % tic & toc to find how long the optimisation takes
+% Use options to set algorithem to SQP
+options = optimoptions('fmincon','Display','iter','Algorithm','sqp'); 
+x0 = [1.5,30,0.10]; % Starting values set
 A = [];
 b = [];
 Aeq = [];
 beq = [];
-lb = [0,15,0];
+lb = [0,15,0]; % Upper and lower bounds of parameters
 ub = [5,76,0.2];
 nonlcon = [];
+%Run Optimiser with CoasterLoop function
 [x] = fmincon(@CoasterLoop,x0,A,b,Aeq,beq,lb,ub,nonlcon,options)
 toc
-% C - 0 - 5
-% Height - 0 - (Ollie)76m
-% Max curvature - 0 - 0.2 
-
-%x = [4.9,34.5]
-%CoasterFunctionX(x)
-% Unsafe isn't working - DONE
-% Current Workflow
-% Report
-% Optimises - DONE
-% - Safe G force plot to match  - DONE
-% - Math verification - Match G to Time - DONE
-% - Multi-Variable Curve
-% - Intergrate the Lenght value - DONE
-% - Boundry Conditions: 
-%   - Max Curvature
-%   - Energy Losses
-
-% In: C, C adjustment values, Height, 
-% Out: Minmised space up to the G Forec Safe plot
-% Constraints:
-% - Safety plot, Max height
-% Constrained Gradient-based Optimisation
